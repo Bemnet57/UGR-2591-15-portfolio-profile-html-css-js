3ce0a7ac9ffd219b5304998d3dc26a126e3ad4f9
Bemnet57 <bemnetasseged57@gmail.com>
2025-01-11 20:14:59 +0300

    added styles and JavaScript

---
+++ src/JS/menu.js
@@ -0,0 +1,5 @@
+// Toggle the visibility of the navigation menu
+document.getElementById('hamburger-menu').addEventListener('click', function () {
+    const navBar = document.getElementById('nav_bar');
+    navBar.classList.toggle('show');
+});
